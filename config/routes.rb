7bb4ac1ea70cb71e88b7c725e510d46455492dbc
Evan Acosta <2000evan@gmail.com>
2015-11-13 19:46:17 +0000

    Added cog and changed main font to Elite Hacker (Corroded)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :contacts
   get '/about' => 'pages#about'
   get '/secret' => 'pages#secret'
+  get '/new' => 'pages#new'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
