d286148bce55b048a6a0665493aaa3fe95b61cc4
Evan Acosta <2000evan@gmail.com>
2015-11-20 21:53:58 +0000

    Added doge page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
   get '/secret' => 'pages#secret'
   get '/new' => 'pages#new'
+  get '/muchwow' => 'pages#muchwow'
+  
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
